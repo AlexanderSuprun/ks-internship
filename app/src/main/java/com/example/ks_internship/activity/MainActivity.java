fb9d83b7a3483530d2c3214c1b18a98b64e57716
a.suprun <alexander.suprun1@gmail.com>
2020-07-10 00:50:26 +0300

    added first activity

---
+++ app/src/main/java/com/example/ks_internship/activity/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.ks_internship.activity;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.ks_internship.R;
+import com.example.ks_internship.utils.Constants;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppCompatEditText editText;
+    private AppCompatButton btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        setListeners();
+    }
+
+    private void initViews() {
+        editText = findViewById(R.id.activity_main_edit_text);
+        btnSend = findViewById(R.id.activity_main_btn_send);
+    }
+
+    private void setListeners() {
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSecondActivityForResult();
+            }
+        });
+    }
+
+    private void openSecondActivityForResult() {
+        if (TextUtils.isEmpty(editText.getText())) {
+            Toast.makeText(MainActivity.this,
+                    getString(R.string.toast_empty_text_field_text), Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra(Constants.EXTRA_MESSAGE, editText.getText().toString());
+        startActivityForResult(intent, Constants.REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+}
---
+++ app/src/main/java/com/example/ks_internship/activity/SecondActivity.java
@@ -0,0 +1,7 @@
+package com.example.ks_internship.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+class SecondActivity extends AppCompatActivity {
+
+}
