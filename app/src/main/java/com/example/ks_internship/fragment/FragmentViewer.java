5b2f1d8e5a94ba8c6a3c23a43ac0bd159be5c0bf
a.suprun <alexander.suprun1@gmail.com>
2020-08-06 18:37:25 +0300

    fixed repo title TextView; added progress bar to WebView

--- app/src/main/java/com/example/ks_internship/fragment/FragmentViewer.java
+++ app/src/main/java/com/example/ks_internship/fragment/FragmentViewer.java
@@ -9,7 +9,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.example.ks_internship.R;
 
@@ -19,6 +22,7 @@ import com.example.ks_internship.R;
 public class FragmentViewer extends Fragment {
 
     WebView webView;
+    ProgressBar progressBar;
 
     public FragmentViewer() {
         // Required empty public constructor
@@ -35,6 +39,7 @@ public class FragmentViewer extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_viewer, container, false);
         webView = view.findViewById(R.id.fragment_viewer_webview);
+        progressBar = view.findViewById(R.id.fragment_viewer_pb);
 
         return view;
     }
@@ -43,7 +48,21 @@ public class FragmentViewer extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
 
+                if(newProgress < 100 && progressBar.getVisibility() == ProgressBar.GONE){
+                    progressBar.setVisibility(ProgressBar.VISIBLE);
+                }
+                progressBar.setProgress(newProgress);
+
+                if(newProgress == 100) {
+                    progressBar.setVisibility(ProgressBar.GONE);
+                }
+            }
+        });
     }
 
     public void loadWebview(String url) {
