126aab5fbfa83412523a2edf5c3032fda8177782
a.suprun <alexander.suprun1@gmail.com>
2020-08-13 00:13:20 +0300

    lesson 10; deleted fragment directory

--- app/src/main/java/com/example/ks_internship/activity/MainActivity.java
+++ app/src/main/java/com/example/ks_internship/activity/MainActivity.java
@@ -7,18 +7,11 @@ import androidx.annotation.Nullable;
 
 import com.example.ks_internship.R;
 import com.example.ks_internship.base.BaseActivity;
-import com.example.ks_internship.fragment.FragmentChooser;
-import com.example.ks_internship.fragment.FragmentViewer;
 import com.example.ks_internship.screen.main.MainContract;
 import com.example.ks_internship.screen.main.MainFragment;
 import com.example.ks_internship.screen.main.MainPresenter;
 import com.example.ks_internship.utils.AppPrefsManager;
 
-/**
- * If in portrait mode, contains {@link FragmentChooser} and starts
- * {@link WebViewActivity} with {@link FragmentViewer} in it.
- * Contains both {@link FragmentChooser} and {@link FragmentViewer}, if in landscape mode.
- */
 
 public class MainActivity extends BaseActivity {
 
@@ -47,86 +40,5 @@ public class MainActivity extends BaseActivity {
         }
 
     }
-//        initToolbarWithHistoryAction(getString(R.string.app_name));
-
-//        adapter = new GitRepoRecyclerAdapter(items, new OnGitRepoRecyclerItemClickListener() {
-//            @Override
-//            public void onItemClick(View v, int position, Uri url) {
-//                openRepo(url);
-//            }
-//        });
-//
-//
-//        inLandscapeMode = getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
-//        fragmentChooser = (FragmentChooser) getSupportFragmentManager().findFragmentById(R.id.activity_main_fragment_chooser);
-//
-//        if (fragmentChooser != null) {
-//            fragmentChooser.setAdapter(adapter);
-//            fragmentChooser.setSearchListener(new OnGitRepoSearchAction() {
-//                @Override
-//                public void onSearchButtonClick(AppCompatEditText usernameInput) {
-//                    AppPrefsManager.cacheSearchHistory(MainActivity.this, usernameInput.getText().toString());
-//                    loadRepos(usernameInput.getText().toString());
-//                }
-//            });
-//        }
-//        if (inLandscapeMode) {
-//            fragmentViewer = (FragmentViewer) getSupportFragmentManager().findFragmentById(R.id.activity_main_fragment_viewer);
-//        }
-//
-//        getDatabase().repoItemDao().getAllRecords().observe(this, new Observer<List<GitRepoItem>>() {
-//            @Override
-//            public void onChanged(List<GitRepoItem> gitRepoItems) {
-//                items.clear();
-//                items.addAll(gitRepoItems);
-//                adapter.notifyDataSetChanged();
-//            }
-//        });
-//
-//        if (getIntent().getStringExtra(Constants.HISTORY_TITLE) != null) {
-//            String username = getIntent().getStringExtra(Constants.HISTORY_TITLE);
-//            loadRepos(username);
-//            fragmentChooser.setUsernameInput(username);
-//        }
-
-//    private void openRepo(Uri url) {
-//        if (inLandscapeMode) {
-//            fragmentViewer.loadWebview(url.toString());
-//        } else {
-//            Intent intent = new Intent(MainActivity.this, WebViewActivity.class);
-//            intent.putExtra(Constants.REPO_URL, url.toString());
-//            startActivity(intent);
-//        }
-//    }
-//
-//    private void loadRepos(String username) {
-//        fragmentChooser.showLoaderBlock();
-//        RestClient.getInstance().getService().searchReposByUsername(username).enqueue(new ApiCallback<List<GitRepoItem>>() {
-//            @Override
-//            public void success(Response<List<GitRepoItem>> response) {
-//                updateList(response.body());
-//                fragmentChooser.hideLoaderBlock();
-//            }
-//
-//            @Override
-//            public void failure(GitRepoError gitRepoError) {
-//                if (TextUtils.isEmpty(gitRepoError.getDocURL())) {
-//                    makeErrorToast(gitRepoError.getMessage());
-//                } else {
-//                    makeErrorToast(gitRepoError.getMessage() + "Details: " + gitRepoError.getDocURL());
-//                }
-//                fragmentChooser.hideLoaderBlock();
-//            }
-//        });
-//    }
-//
-//    private void updateList(List<GitRepoItem> itemsToUpdate) {
-//        getDatabase().repoItemDao().deleteAllRecords();
-//        getDatabase().repoItemDao().insert(itemsToUpdate);
-//    }
-//
-//    private void makeErrorToast(String errorMessage) {
-//        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
-//    }
 
 }
